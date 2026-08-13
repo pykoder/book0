85420b837338b8017a32d3b6d9165181065670fb
Christophe Grosjean <pykoder@gmail.com>
2026-08-13 18:12:52 +0200

    revert: undo out-of-scope asgi.py fix bundled into Task 4 (belongs to Task 5)

--- src/book0_api/asgi.py
+++ src/book0_api/asgi.py
@@ -5,5 +5,4 @@ from book0_api.cli import CONFIG_ENV_VAR
 from book0_api.main import create_app
 from book0_config.config import load_libraries
 
-config = load_libraries(Path(os.environ[CONFIG_ENV_VAR]))
-app = create_app(config.libraries)
+app = create_app(load_libraries(Path(os.environ[CONFIG_ENV_VAR])))
